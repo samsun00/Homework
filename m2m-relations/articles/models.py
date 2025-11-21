b749f7c0e8b05a417184ed5b0db92a52bae4c924
Yura Ushakov <fromura@gmail.com>
2025-11-21 17:04:56 +0300

    m2m-relation-edit

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
